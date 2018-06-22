850f6199b97364c89e756feaa93ef30400236e61
dogabaris <dogabarisozdemir@gmail.com>
2018-06-22 20:22:43 +0300

    Ekran kayan yazıları ayarlandı. Röle bağlantısı için analog pinler kullanıldı. Sms atma durumu çözüldü.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-Nextion Rx --> digital(pwm) PIN11 Uno 
-        Tx --> digital(pwm) PIN10 Uno 
+Nextion Rx --> digital(pwm) PIN13 Uno 
+        Tx --> digital(pwm) PIN12 Uno 
+        gprsSerial kartın dijital pinlerini seri dinlemek için kullanılır.
+        sht11 sıcaklık nem sensörüdür. 
+        HMISerial ekrandır
+        Nextion 5" ekran dokunmatik bulunmaktadır.
+        screen.HMI nextion idesinin ekran için yazılan koddur. Nextion Ideden bakılabilir.
+        Ekrana sdkart ile ttf formatında tasarım aktarılır.
+        Dijital pinler seri portmuş gibi çalışıtırlımak zorundadır çünkü rx0 tx0 bilgisayardan kod aktarılırken veya seri port dinlenirken,
+        diğer seri port dinleyenlerle karışabilmektedir.
+Arduino libraryleri conf klasöründedir
+Kütüphanelerin
+C://users/kişi adı/belgeler/arduino/library klasörüne atılması gerekir.
--- main/main.ino
+++ main/main.ino
@@ -1,36 +1,40 @@
 #include "Nextion.h"
 #include <String.h>
 #include <SHT1x.h>
-#define dataPin 12
-#define clockPin 13
+#define dataPin 10
+#define clockPin 11
 SHT1x sht1x(dataPin, clockPin);
-SoftwareSerial HMISerial(10, 11);
+SoftwareSerial HMISerial(13, 12);
 SoftwareSerial gprsSerial(8,7);
 
-NexText t0 = NexText(0, 2, "t0");
 NexNumber n0 = NexNumber(0, 9, "n0");
 NexNumber n1 = NexNumber(0, 10, "n1");
 NexNumber n2 = NexNumber(0, 14, "n2");
 NexNumber n3 = NexNumber(0, 15, "n3");
 NexText t16 = NexText(0, 27, "t16");
+NexText t0 = NexText(0, 31, "t0");
+NexText t17 = NexText(0, 32, "t17");
+NexText t18 = NexText(0, 33, "t18");
 NexText g0 = NexText(0, 26, "g0");
-NexText t13 = NexText(0, 23, "t13");
-NexText t12 = NexText(0, 22, "t12");
+NexText g1 = NexText(0, 28, "g1");
+NexText g2 = NexText(0, 29, "g2");
+NexText g3 = NexText(0, 30, "g3");
 NexText t11 = NexText(0, 21, "t11");
-NexText t10 = NexText(0, 20, "t10");
 NexButton b0 = NexButton(0, 5, "b0");
 NexButton b1 = NexButton(0, 6, "b1");
 NexButton b2 = NexButton(0, 7, "b2");
 NexButton b3 = NexButton(0, 8, "b3");
-NexDSButton bt0 = NexDSButton(0, 2, "bt0");
 
-NexTouch *nex_listen_list[] = {&bt0, &b0,&b1,&b2,&b3, NULL};
+byte ter[3] = {255,255,255};
+bool sendMessageActive = true;
+NexTouch *nex_listen_list[] = { &b0,&b1,&b2,&b3, NULL};
 int sicaklik = 0;
 int nem = 0;
 int inekSayisi = 0;
 int toplamSut = 0;
 float sni = 0;
 unsigned long lastTime = 0;
+unsigned long sendMessagelastTime = 0;
 float calculateSni(int sicaklik, int nem){
   sni = (1.8*sicaklik + 32)-(0.55 - 0.0055 * nem)*(1.8*sicaklik-26);
   char sniStr[8];
@@ -39,24 +43,7 @@ float calculateSni(int sicaklik, int nem){
 }
 float tempC = 0;
 float humidity = 0;
-void bt0PopCallback(void *ptr)
-{
-  uint32_t dual_state;
-  NexDSButton *btn = (NexDSButton *)ptr;
-  dbSerialPrintln("b0PopCallback");
-  dbSerialPrint("ptr=");
-  dbSerialPrintln((uint32_t)ptr);
-  bt0.getValue(&dual_state);
-  
-  if (dual_state)               
-  {
-    digitalWrite(13, HIGH);           // if my dual state buton is pressed
-  }
-  else
-  {
-    digitalWrite(13, LOW);            // if not
-  }
-}
+
 void b0PopCallback(void *ptr)
 {
   dbSerialPrintln("b0PopCallback");
@@ -90,17 +77,45 @@ void b3PopCallback(void *ptr)
 
 void setup(void)
 {
-  pinMode(13, OUTPUT);
+  gprsSerial.begin(19200);
+  Serial.begin(19200);
+  delay(500);
+  pinMode(A1, OUTPUT);
+  analogWrite(A1, 255);
+  pinMode(A2, OUTPUT);
+  analogWrite(A2, 255);
+  pinMode(A3, OUTPUT);
+  analogWrite(A3, 255);
+  pinMode(A4, OUTPUT);
+  analogWrite(A4, 255);
+
+ 
+  
   nexInit();
   n0.setValue(inekSayisi);
   n1.setValue(toplamSut);
   
-  bt0.attachPop(bt0PopCallback, &bt0);
   b0.attachPop(b0PopCallback, &b0);
   b1.attachPop(b1PopCallback, &b1);
   b2.attachPop(b2PopCallback, &b2);
   b3.attachPop(b3PopCallback, &b3);
   //dbSerialPrintln("setup done");
+  nexSerial.print("vis t16,0");
+  nexSerial.write(ter,3);
+  nexSerial.print("vis t0,0");
+  nexSerial.write(ter,3);
+  nexSerial.print("vis t17,0");
+  nexSerial.write(ter,3);
+  nexSerial.print("vis t18,0");
+  nexSerial.write(ter,3);
+  nexSerial.print("vis g0,0");
+  nexSerial.write(ter,3);
+  nexSerial.print("vis g1,0");
+  nexSerial.write(ter,3);
+  nexSerial.print("vis g2,0");
+  nexSerial.write(ter,3);
+  nexSerial.print("vis g3,0");
+  nexSerial.write(ter,3);
 }
 void setSicaklik(float sicaklik){
   n2.setValue(sicaklik);
@@ -108,45 +123,123 @@ void setSicaklik(float sicaklik){
 void setNem(float nem){
   n3.setValue(nem);
 }
-void SendTextMessage()
+void SendTextMessage(int stresSeviyesi)
 {
-  Serial.println("Sending Text...");
-  gprsSerial.print("AT+CMGF=1\r"); // Set the shield to SMS mode
-  delay(100);
-  gprsSerial.println("AT+CMGS = \"+0905433413415\"");
-  delay(100);
-  gprsSerial.println("How are you today?"); //the content of the message
-  delay(100);
-  gprsSerial.print((char)26);//the ASCII code of the ctrl+z is 26 (required according to the datasheet)
-  delay(100);
-  gprsSerial.println();
-  Serial.println("Text Sent.");
-}
-void DialVoiceCall()
-{
-  gprsSerial.println("ATD+xxxxxxx;");//dial the number, must include country code
-  delay(100);
-  gprsSerial.println();
+  if(sendMessageActive==true){
+    Serial.println("Sending Text...");
+    gprsSerial.print("AT+CMGF=1\r"); // Set the shield to SMS mode
+    delay(100);
+    gprsSerial.println("AT+CMGS=\"+905433413415\"");
+    delay(100);
+    if(stresSeviyesi==1)
+      gprsSerial.println("Alarm");
+    else if(stresSeviyesi==2)
+      gprsSerial.println("Tehlike baslangici");
+    else if(stresSeviyesi==3)
+      gprsSerial.println("Acil durum");
+    delay(100);
+    gprsSerial.print((char)26);//the ASCII code of the ctrl+z is 26 (required according to the datasheet)
+    delay(100);
+    gprsSerial.println();
+    Serial.println("Text Sent.");
+    sendMessageActive = false;
+  }
 }
 
 void loop(void)
 {
-  if (Serial.available()) // if there is incoming serial data
-   switch(Serial.read()) // read the character
-   {
-     case 't': // if the character is 't'
-       SendTextMessage(); // send the text message
-       break;
-     case 'd': // if the character is 'd'
-       DialVoiceCall(); // dial a number 
-       break;
-   } 
- 
+  if (Serial.available()){// if there is incoming serial data
+    switch(Serial.read()) // read the character
+     {
+       case 't': // if the character is 't'
+         SendTextMessage(1); // send the text message
+         break;
+     } 
+  }
+   
   if (gprsSerial.available()){ // if the shield has something to say
     Serial.write(gprsSerial.read()); // display the output of the shield
   }
-  
+  if(sni<72){
+    nexSerial.print("vis t16,1");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g0,1");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t0,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g1,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t17,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g2,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t18,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g3,0");
+    nexSerial.write(ter,3);
+  }
+  else if(sni>=72 && sni<80){
+    nexSerial.print("vis t16,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g0,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t0,1");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g1,1");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t17,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g2,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t18,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g3,0");
+    nexSerial.write(ter,3);
+    SendTextMessage(1);
+  }
+  else if(sni>=80 && sni<90){
+    nexSerial.print("vis t16,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g0,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t0,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g1,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t17,1");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g2,1");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t18,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g3,0");
+    nexSerial.write(ter,3);
+    SendTextMessage(2);
+  }
+  else if(sni>=90){
+    nexSerial.print("vis t16,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g0,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t0,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g1,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t17,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g2,0");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis t18,1");
+    nexSerial.write(ter,3);
+    nexSerial.print("vis g3,1");
+    nexSerial.write(ter,3);
+    SendTextMessage(3);
+  }
   nexLoop(nex_listen_list);
+  if(millis() - sendMessagelastTime > 900000){ //15 dk
+    sendMessageActive = true;
+    sendMessagelastTime = millis();
+  }
   if (millis() - lastTime > 1000)//1sn
   {
     tempC = sht1x.readTemperatureC();
